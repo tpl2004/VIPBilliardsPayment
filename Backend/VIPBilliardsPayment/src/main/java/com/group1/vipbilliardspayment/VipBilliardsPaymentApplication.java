5812e0942acda4f2a38bc1389cb6e6cfeca059aa
Truong Loc <truongloc240724@gmail.com>
2025-04-01 22:48:35 +0700

    Create Spring boot project

---
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/VipBilliardsPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.group1.vipbilliardspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipBilliardsPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipBilliardsPaymentApplication.class, args);
+	}
+
+}
---
+++ Backend/VIPBilliardsPayment/src/test/java/com/group1/vipbilliardspayment/VipBilliardsPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.vipbilliardspayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipBilliardsPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
