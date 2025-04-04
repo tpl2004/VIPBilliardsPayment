0a15f7f10dbffaa0c297765e09521269691870a5
truongloc <truongloc240724@gmail.com>
2025-04-05 01:11:48 +0700

    add: adding annotation @Repository to CapDoHoiVienRepository and HoiVienRepository

--- Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/CapDoHoiVienRepository.java
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/CapDoHoiVienRepository.java
@@ -2,6 +2,8 @@ package com.group1.vipbilliardspayment.repository;
 
 import com.group1.vipbilliardspayment.entity.CapDoHoiVien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CapDoHoiVienRepository extends JpaRepository<CapDoHoiVien, Integer> {
 }
--- Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/HoiVienRepository.java
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/HoiVienRepository.java
@@ -2,6 +2,8 @@ package com.group1.vipbilliardspayment.repository;
 
 import com.group1.vipbilliardspayment.entity.HoiVien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HoiVienRepository extends JpaRepository<HoiVien, Integer> {
 }
