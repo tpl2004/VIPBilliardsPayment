a8c8a974efd0d3102944dd674a9e2a67461ecbb3
truongloc247 <truongloc240724@gmail.com>
2025-04-05 00:52:30 +0700

    Revert "add: some entities (NguoiDung, ChuQuan, ThuNgan) and some repositories (ChuQuanRepository, ThuNganRepository)"

--- Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/ThuNganRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group1.vipbilliardspayment.repository;
-
-import com.group1.vipbilliardspayment.entity.ThuNgan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ThuNganRepository extends JpaRepository<ThuNgan, Integer> {
-}
