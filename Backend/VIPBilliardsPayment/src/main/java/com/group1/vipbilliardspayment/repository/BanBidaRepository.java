560f09ea8de6d2640a689b9943371621e205d8f5
nguyễnquoc123 <quoc4373@gmail.com>
2025-04-05 12:22:18 +0700

    lam lai

---
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/BanBidaRepository.java
@@ -0,0 +1,10 @@
+package com.group1.vipbilliardspayment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group1.vipbilliardspayment.entity.BanBida;
+@Repository
+public interface BanBidaRepository extends JpaRepository<BanBida, Integer>{
+
+}
