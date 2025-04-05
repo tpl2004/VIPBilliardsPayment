6cfcf36d40e9bb127a2ceae376ab9545915620ec
TruongTai <truongtai072424@gmail.com>
2025-04-05 12:31:14 +0700

    add: some request classes and some error code

---
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/dto/request/ThuNganUpdateRequest.java
@@ -0,0 +1,40 @@
+package com.group1.vipbilliardspayment.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ThuNganUpdateRequest {
+    @NotNull(message = "FIELD_NULL")
+    String hoTen;
+
+    @NotNull(message = "FIELD_NULL")
+    Date ngaySinh;
+
+    @NotNull(message = "FIELD_NULL")
+    boolean gioiTinhNu;
+
+    @NotNull(message = "FIELD_NULL")
+    @Email(message = "EMAIL_INVALID")
+    String email;
+
+    @NotNull(message = "FIELD_NULL")
+    @Pattern(regexp = "^0[0-9]{9}$", message = "SODIENTHOAI_INVALID")
+    String soDienThoai;
+
+    @NotNull(message = "FIELD_NULL")
+    @Pattern(regexp = "[0-9]{12}$", message = "SOCCCD_INVALID")
+    String soCCCD;
+
+    @NotNull(message = "FIELD_NULL")
+    @Size(min = 3, message = "MATKHAU_INVALID")
+    String matKhau;
+}
