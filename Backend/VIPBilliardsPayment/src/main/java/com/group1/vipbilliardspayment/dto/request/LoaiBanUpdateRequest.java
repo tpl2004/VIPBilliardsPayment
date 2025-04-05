f0ca6df49c8e2d11f6a170c7edb0c397d90b4c38
TruongTai <truongtai072424@gmail.com>
2025-04-05 13:30:31 +0700

    add: some request classes and some error codes

---
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/dto/request/LoaiBanUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.group1.vipbilliardspayment.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoaiBanUpdateRequest {
+    @NotNull(message = "FIELD_NULL")
+    String tenLoai;
+
+    @NotNull(message = "FIELD_NULL")
+    @Min(value = 0, message = "DONGIA_INVALID")
+    double donGia;
+}
