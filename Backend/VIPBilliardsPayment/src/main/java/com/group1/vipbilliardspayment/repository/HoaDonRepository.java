4e3f3cfb973738960837c228865794860e01984c
TruongTai <truongtai072424@gmail.com>
2025-04-07 18:44:26 +0700

    complete: /hoadon APIs

--- Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/HoaDonRepository.java
+++ Backend/VIPBilliardsPayment/src/main/java/com/group1/vipbilliardspayment/repository/HoaDonRepository.java
@@ -1,11 +1,21 @@
 package com.group1.vipbilliardspayment.repository;
 
+import com.group1.vipbilliardspayment.entity.BanBida;
+import com.group1.vipbilliardspayment.entity.HoiVien;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.group1.vipbilliardspayment.entity.HoaDon;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface HoaDonRepository extends JpaRepository<HoaDon, Integer> {
     public boolean existsByMaHoaDon(Integer maHoaDon);
+
+    public List<HoaDon> findByThoiDiemVao(Date createdDate);
+
+    public List<HoaDon> findByBanBida(BanBida banBida);
 }
