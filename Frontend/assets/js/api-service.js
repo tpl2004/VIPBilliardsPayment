5a7c5ec6700e17f5f11abcbd944bc1bd64ca6ec5
Phuoc BTY <phuocpham044@gmail.com>
2025-04-10 23:01:20 +0700

    add: handle login for thu ngan

---
+++ Frontend/assets/js/api-service.js
@@ -0,0 +1,23 @@
+import { host } from "./config.js";
+
+const loaiBanApi = host + '/loaiban';
+const banBidaApi = host + '/banbida';
+const hoaDonApi = host + '/hoadon';
+const matHangApi = host + '/mathang';
+const matHangTrongHoaDonApi = host + '/mathangtronghoadon';
+const hoiVienApi = host + '/hoivien';
+const capDoHoiVienApi = host + '/capdohoivien';
+const thuNganApi = host + '/thungan';
+const authApi = host + '/authentication';
+
+export {
+    loaiBanApi,
+    banBidaApi,
+    hoaDonApi,
+    matHangApi,
+    matHangTrongHoaDonApi,
+    hoiVienApi,
+    capDoHoiVienApi,
+    thuNganApi,
+    authApi,
+};
---
+++ Frontend/assets/js/index.js
@@ -0,0 +1,93 @@
+import { localStorageUserTokenKey } from './config.js';
+import * as api from './api-service.js'
+
+var usernameInput = document.querySelector('input[name = "username"]');
+var passwordInput = document.querySelector('input[name = "password"]');
+var loginBtn = document.querySelector('button');
+var username = null;
+var password = null;
+
+function main() {
+    checkSignedIn()
+    .then(result => {
+        window.location = 'thu-ngan-home.html';
+    })
+    .catch(ms => {
+        console.log(ms);
+        handleEvents();
+    })
+}
+
+main();
+
+// check signed in
+function checkSignedIn() {
+    const USER_TOKEN = localStorage.getItem(localStorageUserTokenKey);
+    return new Promise((resolve, reject) => {
+        if(!USER_TOKEN) {
+            reject("not signed in");
+        } else {
+            var options = {
+                method: 'POST',
+
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+
+                body: JSON.stringify({
+                    "token": USER_TOKEN
+                }),
+            }
+            fetch(api.authApi + '/introspect', options)
+            .then(response => response.json())
+            .then(response => {
+                if(response.code == 1000 && response.result.valid) resolve("signed in");
+                else reject("not signed in");
+            })
+            .catch(err => {
+                console.error(err);
+                reject("cannot introspect token");
+            })
+        }
+    })
+}
+
+// handle events
+function handleEvents() {
+
+    usernameInput.addEventListener('change', e => {
+        username = usernameInput.value;
+        console.log(username);
+    })
+    
+    passwordInput.addEventListener('change', e => {
+        password = passwordInput.value;
+        console.log(password);
+    })
+    
+    loginBtn.addEventListener('click', e => {
+        var options = {
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+            body: JSON.stringify({
+                "tenDangNhap": username,
+                "matKhau": password
+            }),
+        }
+        fetch(api.authApi + "/thungantoken", options)
+        .then(response => response.json())
+        .then(response => {
+            if(response.code == 1000) {
+                localStorage.setItem(localStorageUserTokenKey, response.result.token);
+                window.location = 'thu-ngan-home.html';
+            }
+            else {
+                alert(response.message);
+            }
+        })
+    })
+}
