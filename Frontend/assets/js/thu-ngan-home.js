76c7fdc861e62e7e1abb736f2781d516fb474ab1
truongloc <truongloc240724@gmail.com>
2025-04-12 13:01:29 +0700

    update: css for thu ngan home page

--- Frontend/assets/js/thu-ngan-home.js
+++ Frontend/assets/js/thu-ngan-home.js
@@ -4,12 +4,28 @@ import * as api from './api-service.js'
 const TOKEN = localStorage.getItem(localStorageUserTokenKey);
 
 var logoutBtn = document.querySelector('button[class = "logout"]');
+var bodyBanBidaListBox = document.querySelector('.extension .content .ban-bida-list .body-ban-bida-list');
+var showBidaTableListBtn = document.getElementById('show-bida-table-list');
+var showBillListBtn = document.getElementById('show-bill-list');
+var showMemberList = document.getElementById('show-member-list');
+var functionTaskbars = [showBidaTableListBtn, showBillListBtn, showMemberList];
+var xemDanhSachBanBidaExtendFuncsBox = document.querySelector('.xem-danh-sach-ban-bida');
+var xemDanhSachHoiVienExtendFuncsBox = document.querySelector('.xem-danh-sach-hoi-vien');
+var extendFunctionGroups = [xemDanhSachBanBidaExtendFuncsBox, xemDanhSachHoiVienExtendFuncsBox];
+console.log(extendFunctionGroups)
 
 function main() {
 
     checkSignedIn()
     .then(response => {
-        handleEvents();
+        activeMainFuntion('show-bida-table-list');
+        enableExtendFuncGroup('xem-danh-sach-ban-bida');
+        getAllBanBidaChuaXoa()
+        .then(response => response.json())
+        .then(response => {
+            renderBanBidas(response.result);
+        })
+        .then(handleEvents);
     })
     .catch(ms => {
         window.location = 'index.html';
@@ -49,6 +65,63 @@ function checkSignedIn() {
     })
 }
 
+// get all ban bida chua xoa
+function getAllBanBidaChuaXoa() {
+    var options = {
+        method: 'GET',
+
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        },
+    }
+    return fetch(api.banBidaApi + '/getallbanbidachuaxoa', options);
+}
+
+// render nhung ban bida len mang  hinh
+function renderBanBidas(banBidas) {
+    var html = banBidas.map((banBida, index) => {
+        return `
+            <div class="ban-bida">
+                <p>${banBida.soBan}</p>
+                <p>${banBida.trangThai}</p>
+                <p>${banBida.tenLoaiBan}</p>
+                <p>${banBida.donGia}</p>
+            </div>
+        `
+    })
+    bodyBanBidaListBox.innerHTML = html.join('');
+}
+
+// active a main function
+function activeMainFuntion(funcId) {
+    var func = document.getElementById(funcId);
+    if(!func.classList.contains('active')) {
+        func.classList.add('active');
+    }
+    functionTaskbars.forEach(func => {
+        if(func.id != funcId && func.classList.contains('active')) {
+            func.classList.remove('active');
+        }
+    })
+}
+
+// enable a extend function group
+function enableExtendFuncGroup(GroupFuncClass = null) {
+    if(GroupFuncClass) {
+        var GroupFunc = document.querySelector('.'+GroupFuncClass);
+        if(GroupFunc.classList.contains('disable')) {
+            GroupFunc.classList.remove('disable');
+        }
+    }
+    extendFunctionGroups.forEach(extendFuncGroup => {
+        if(extendFuncGroup.className != GroupFuncClass && !extendFuncGroup.classList.contains('disable')) {
+            extendFuncGroup.classList.add('disable');
+        }
+        // if(extendFuncGroup.className != GroupFuncClass) console.log(extendFuncGroup);
+    })
+}
+
 // handle events
 function handleEvents() {
 
@@ -59,5 +132,20 @@ function handleEvents() {
             localStorage.removeItem(localStorageUserTokenKey);
             window.location.reload();
         }
+    })
+    
+    showBidaTableListBtn.addEventListener('click', e => {
+        activeMainFuntion(showBidaTableListBtn.id);
+        enableExtendFuncGroup('xem-danh-sach-ban-bida')
+    })
+    
+    showBillListBtn.addEventListener('click', e => {
+        activeMainFuntion(showBillListBtn.id);
+        enableExtendFuncGroup(null);
+    })
+    
+    showMemberList.addEventListener('click', e => {
+        activeMainFuntion(showMemberList.id);
+        enableExtendFuncGroup('xem-danh-sach-hoi-vien');
     })
 }
